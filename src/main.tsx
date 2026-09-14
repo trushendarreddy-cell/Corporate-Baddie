236030033824e06cb72d6ae2bb3f68148c086948
Thumati Rushendar Reddy <trushendarreddy@gmail.com>
2026-09-14 23:07:12 +0530

    Launch CorporateBaddie through interactive 3D intro

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import {StrictMode} from 'react';
+import {StrictMode, useState} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import IntroExperience from './components/IntroExperience';
 import './index.css';
+
+function Root() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  if (showIntro) {
+    return <IntroExperience onEnter={() => setShowIntro(false)} />;
+  }
+
+  return <App />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 );
